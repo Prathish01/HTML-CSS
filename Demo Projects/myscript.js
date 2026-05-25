9e3d5472faf91f344fe98519b51919c76a654e0d
Prathish S <prathish472@gmail.com>
2026-05-25 17:51:40 +0530

    Update myscript.js

--- Demo Projects/myscript.js
+++ Demo Projects/myscript.js
@@ -4,29 +4,38 @@ function ShowCourses()
     .then((response) => response.json())
     .then((courses) =>
     {
+        
         const datatable = document.getElementById("CourseList")
         let row = " "
         courses.forEach(course => 
+            
             {
             row += `<tr> 
+            
             <td>${course.courseId}</td>
             <td>${course.courseName}</td>
             <td>${course.trainer}</td>
             <td>${course.durationInWeeks}</td>
             </tr>`
+                
         });
         datatable.innerHTML += row 
     })
+    
 }
 function Enrolledstudents()
+    
 {
 fetch("http://localhost:8080/courses/enrolled")
+    
 .then((Response) => Response.json())
 .then((students) =>
 {
+    
 const stulist = document.getElementById("EnrolledList");
 var row1 = ""
 students.forEach(student =>
+    
 {
       row1 += `<tr>
      <td>${student.id}</td>
