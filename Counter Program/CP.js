a63d20f5c79a60e46121a0331e4aafa77c346193
Prathish S <prathish472@gmail.com>
2026-06-15 17:58:25 +0530

    Update CP.js

--- Counter Program/CP.js
+++ Counter Program/CP.js
@@ -4,11 +4,14 @@ const increaseBtn =document.getElementById("increaseBtn");
 const countLabel =document.getElementById("countLabel");
 let count =0;
 
+
 increaseBtn.onclick = function(){
     count++;
     countLabel.textContent = count;
 
+    
 }
+
 decreaseBtn.onclick =function(){
     count--;
     countLabel.textContent=count;
